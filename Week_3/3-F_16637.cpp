2ec563d32062d73c751cc0ff607a85ef6b07a0d6
Kim Taebum <ddino99@naver.com>
2024-06-29 17:11:13 +0900

    3-H

--- Week_3/3-F_16637.cpp
+++ Week_3/3-F_16637.cpp
@@ -1,56 +1,43 @@
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
+using namespace std;  
 
-int n;
-string s;
-vector<int> nums;
-vector<char> opers;
-ll answer;
-
-ll carculate(int a, int b, char op)
-{
-    if (op == '+')
-    {
-        return a + b;
-    }
-    if (op == '-')
-    {
-        return a - b;
-    }
-    if (op == '*')
-    {
-        return a * b;
-    }
-}
-
-int main() {
+vector<int> num; 
+vector<char> oper_str; 
+int n, ret = -987654321;
+string s; 
+void fastIO(){
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(NULL); 
+    cout.tie(NULL);   
+} 
+int oper(char a, int b, int c){
+    if(a == '+') return b + c; 
+    if(a == '-') return b - c; 
+    if(a == '*') return b * c;  
+} 
 
-    cin >> n;
-    cin >> s;
+void go(int here, int _num){
+    if(here == num.size() - 1){ 
+        ret = max(ret, _num); 
+        return;
+    }  
+    go(here + 1, oper(oper_str[here], _num, num[here + 1]));
 
-    for(int i = 0; i < n; i++){
-        if(i % 2 == 0){
-            nums.push_back(s[i] - '0');
-        }
-        else{
-            opers.push_back(s[i]);
-        }
+    if(here + 2 <= num.size() - 1){
+        int temp = oper(oper_str[here + 1], num[here + 1], num[here + 2]); 
+        go(here + 2, oper(oper_str[here], _num, temp));  
     } 
-
-    for (int i = 0; i < opers.size(); i++)
-    {
-        nums[i] = carculate(nums[i], nums[i + 1], opers[i]);
-        
-        int temp = nums[0];
-        for (int j = 0; j < opers.size(); j++)
-        {
-            temp = carculate(temp, nums[i + 1], opers[i]);
-        }
-    }
-
+    return;
+} 
+int main(){
+    fastIO();
+    cin >> n;  
+    cin >> s; 
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0)num.push_back(s[i] - '0');
+        else oper_str.push_back(s[i]);
+    } 
+    go(0, num[0]);  
+    cout << ret << "\n";
     return 0;
-}
+} 
