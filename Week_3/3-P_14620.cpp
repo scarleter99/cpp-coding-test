7cef2a26d3e4685e6e14b575a9461ade23252ec2
Kim Taebum <ddino99@naver.com>
2024-07-08 19:57:20 +0900

    3-Q

--- Week_3/3-P_14620.cpp
+++ Week_3/3-P_14620.cpp
@@ -1,105 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int n, ret=987654321, v[20][20], p[20][20], dy[4] = { -1,1,0,0 }, dx[4] = {0,0,-1,1};
 
-int n;
-int adj[14][14], visited[14][14];
-int answer = 987654321;
-
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-
-int plusCost(int y, int x){
-    int cost = adj[y][x];
-    visited[y][x] = 1;
-    for (int i = 0; i < 4; i++){
+int setFlower(int y, int x){ 
+    v[y][x] = 1;
+    int s = p[y][x];
+    for (int i = 0; i < 4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-
-        cost += adj[ny][nx];
-        visited[ny][nx] = 1;
+        v[ny][nx] = 1;
+        s += p[ny][nx];
     }
-
-    return cost;
+    return s; 
 }
-
-int minusCost(int y, int x){
-    int cost = -adj[y][x];
-    visited[y][x] = 0;
-    for (int i = 0; i < 4; i++){
+bool check(int y, int x) { 
+    if (v[y][x]) return 0;
+    for (int i = 0; i < 4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-
-        cost += adj[ny][nx];
-        visited[ny][nx] = 0;
-    }
-
-    return cost;
+        if (ny<0 || ny>=n || nx < 0 || nx >= n || v[ny][nx]) {
+            return 0;
+        } 
+    } 
+    return 1;  
 }
 
-void algo(int y, int x, int cnt, int cost) {
-    cout << "algo: " << y << " " << x  << " " << cnt << " " << cost << "\n";
-
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout << visited[i][j] << " ";
-        }
-        cout << "\n";
+void eraseFlower(int y, int x) {
+    v[y][x] = 0;
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        v[ny][nx] = 0;
     }
-    cout << "\n";
-
-    if (cnt == 3){
-        answer = min(answer, cost);
+}
+void flower(int cnt,int hap) {
+    if (cnt == 3) {
+        ret = min(ret, hap);
         return;
     }
-
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if (visited[i][j]){
-                continue;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) { 
+            if (check(i, j)) {   
+                flower(cnt+1,hap + setFlower(i, j)); 
+                eraseFlower(i, j);
             }
-
-            int flag = 0;
-            for (int k = 0; k < 4; k++){
-                int ny = i + dy[k];
-                int nx = j + dx[k];
-
-                if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx]){
-                    flag = 1;
-                    continue;
-                }
-            }
-            cout << "algo: " << i << " " << j  << " " << flag << "\n";
-            if (flag){
-                continue;
-            }
-            cost += plusCost(i, j);
-            algo(i, j, cnt + 1, cost);
-            cost += minusCost(i, j);
         }
-    }
+    } 
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
+int main()
+{
     cin >> n;
-
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cin >> adj[i][j];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> p[i][j];
         }
     }
-
-    for (int i = 1; i < n - 1; i++){
-        for (int j = 1; j < n - 1; j++){
-            algo(i, j, 1, plusCost(i, j));
-            minusCost(i, j);
-        }
-    }
-
-    cout << answer;
-
-    return 0;
+    flower(0, 0);
+    cout << ret;
 }
